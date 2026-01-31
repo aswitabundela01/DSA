34ba0d3d278d35e2c4a32a9540387b7aae0ccfc5
aswitabundela01 <143839192+aswitabundela01@users.noreply.github.com>
2026-01-31 13:51:39 +0530

    Time: 0 ms (100.00%) | Memory: 48.8 MB (62.85%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -2,16 +2,39 @@ import java.util.*;
 
 class Solution {
     public int removeDuplicates(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int index = 0; 
-        for (int i = 0; i < nums.length; i++) {
-         map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            if (map.get(nums[i]) <= 2) {
-                nums[index] = nums[i];
-                index++;
+
+        int k = 1;
+        int count =1;
+        for(int i =1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                count ++;
+
+            }else{
+                count=1;
             }
+
+        
+if(count<=2){
+    nums[k]= nums[i];
+    k++;
+}
+
         }
 
-        return index;
+return k;
+
+
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // int index = 0; 
+        // for (int i = 0; i < nums.length; i++) {
+        //  map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        //     if (map.get(nums[i]) <= 2) {
+        //         nums[index] = nums[i];
+        //         index++;
+        //     }
+        // }
+
+        // return index;
     }
 }
